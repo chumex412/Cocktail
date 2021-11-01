dff8983da5c0532972853692087dcfbfc9c50d4f
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-11-01 07:10:26 +0100

    Added about us page

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <section className="about-us">
+      <div className="container">
+        <section className="about-us-wrapper">
+          <h2 className="section-title">about us</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+            perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
+            nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
+            hic aspernatur error blanditiis?
+          </p>
+        </section>
+      </div>
+    </section>
+  )
+};
+
+export default About;
