c3ceb33e233a18b218845310f0c1bbd5b59b65e0
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-11-01 07:07:06 +0100

    Loader file

---
+++ src/components/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Loader = () => {
+
+  return (
+    <div className="loader-wrapper">
+      <p className="loader">
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </p>
+    </div>
+  )
+};
+
+export default Loader;
